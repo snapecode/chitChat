befae521d3e592fc67a0dd451ff89095001dab67
Stevie Snape <smasnape@gmail.com>
2018-02-14 03:44:45 -0500

    socket.io and MongoDb

--- server.js
+++ server.js
@@ -1,39 +1,63 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
-
-
+var http = require('http').Server(app);
+var io = require ('socket.io')(http);
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var dbURl = 'mongodb://admin:admin@ds235778.mlab.com:35778/chitchat-node'
+
+var Message = mongoose.model('Message',{
+    name: String,
+    message: String
+})
 
 
-var messages =[
-
-    {name: 'Stevie', message: 'wud up playa playa'},
-    {name: 'Vitt', message: 'not much susucka'},
-    {name: 'Sashman', message: 'How you feeling'},
-    {name: 'Doomer\'s Revenge', message: 'like a sweet onion maaan!!!'}
-]
+// var messages =[   this is for tests of the message display area
+//
+//     {name: 'Stevie', message: 'wud up playa playa'},
+//     {name: 'Vitt', message: 'not much sucka'},
+//     {name: 'Sashman', message: 'How you feeling'},
+//     {name: 'Doomer\'s Revenge', message: 'like a sweet onion maaan!!!'}
+// ]
 
 
 
 app.get('/messages', (req, res) =>{
-    res.send(messages)
+    Message.find({}, (err, messages)=>{
+        res.send(messages)
+
+    })
+
 
 })
 
 app.post('/messages', (req, res) =>{
    // console.log(req.body)    console test
-    messages.push(req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save((err) => {
+        if (err)
+            sendStatus(500)
 
+        // messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
 })
 
+io.on('connection', (socket) => {
+    console.log('New User connected')
+})
+mongoose.connect(dbURl, (err)=> {
+    console.log('mongo db connection', err)
+})
 
-var server = app.listen(3005, () =>{
+var server = http.listen(3006, () => {
 
     console.log('server is listening on port', server.address().port)
 
-});
+})
